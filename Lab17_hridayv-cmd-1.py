5be72238cbf0429a3ddf1a8e82d150c86aa3b83f
Hriday <hridayvermani12@gmail.com>
2026-08-13 19:26:24 -0400

    Updated Docstring

--- Lab17_hridayv-cmd-1.py
+++ Lab17_hridayv-cmd-1.py
@@ -3,7 +3,7 @@ Program Name: Refactored Hacker News API Script
 Author: Hriday Vermani
 Purpose: Fetches top stories from the Hacker News API and safely handles 
          articles missing comment counts ('descendants' key) using .get() 
-         to prevent KeyError crashes before printing.
+         to prevent KeyError crashes before printing from hn_submissions.py from the textbook.
 Starter Code Source: Python Crash Course (Chapter 17)
 Date: August 13, 2026
 """
