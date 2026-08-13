46f29b5e6922ad31eb9488446515604d375138f8
Hriday <hridayvermani12@gmail.com>
2026-08-13 18:55:40 -0400

    Fetch top story IDs from Hacker News API and setup list container

--- Lab17_hridayv-cmd-1.py
+++ Lab17_hridayv-cmd-1.py
@@ -8,3 +8,14 @@ Starter Code Source: Python Crash Course (Chapter 17)
 Date: August 13, 2026
 """
 
+from operator import itemgetter
+import requests
+
+# Make an API call to get top story IDs
+url = "https://hacker-news.firebaseio.com/v0/topstories.json"
+r = requests.get(url)
+print(f"Status code: {r.status_code}")
+
+# Process information about each submission
+submission_ids = r.json()
+submission_dicts = []
